81cb2de8ccc4afef8dbd5bdcc290a688c282ed49
Piotr Andrzejewski <acax.piotrandrzejewski@gmail.com>
2024-08-13 00:15:02 +0200

    update solution with Page Object concept

---
+++ WebDriverTask1/HomePage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+
+namespace WebDriverTask1
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+
+        // Locators
+        private By newPasteTextArea = By.Id("postform-text");
+        private By pasteExpirationDropdown = By.XPath("//span[@id='select2-postform-expiration-container']");
+        private By pasteExpirationOption = By.XPath("//li[text()='10 Minutes']");
+        private By pasteNameInput = By.Id("postform-name");
+        private By createNewPasteButton = By.XPath("//button[contains(text(),'Create New Paste')]");
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl("https://pastebin.com/");
+        }
+
+        public void EnterPasteCode(string code)
+        {
+            driver.FindElement(newPasteTextArea).SendKeys(code);
+        }
+
+        public void SelectPasteExpiration(string expirationTime)
+        {
+            driver.FindElement(pasteExpirationDropdown).Click();
+            driver.FindElement(pasteExpirationOption).Click();
+        }
+
+        public void EnterPasteName(string name)
+        {
+            driver.FindElement(pasteNameInput).SendKeys(name);
+        }
+
+        public void CreateNewPaste()
+        {
+            driver.FindElement(createNewPasteButton).Click();
+        }
+    }
+}
--- WebDriverTask1/Tests.cs
+++ WebDriverTask1/Tests.cs
@@ -1,61 +1,39 @@
-using OpenQA.Selenium.Edge;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
 
 namespace WebDriverTask1
 {
     public class Tests
     {
         private readonly IWebDriver driver;
-        private const string baseUrl = "https://pastebin.com/";
-        private const string content = "Hello from WebDriver";
-        private const string title = "helloweb";
 
         public Tests()
         {
-            EdgeOptions options = new EdgeOptions();
-            options.AddArgument("-inprivate");
-            options.AddArgument("--start-maximized");
-            driver = new EdgeDriver(options);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver = new EdgeDriver();
         }
 
         [Fact]
-        public void CreateNewPasteTest()
+        public void Should_Create_New_Paste_With_Specified_Attributes()
         {
-            driver.Navigate().GoToUrl(baseUrl);
+            // Arrange
+            var pastebinHomePage = new HomePage(driver);
 
-            // Accept cookies
-            driver
-                .FindElement(By.CssSelector("#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.css-47sehv"))
-                .Click();
+            // Act
+            pastebinHomePage.Open();
+            pastebinHomePage.EnterPasteCode("Hello from WebDriver");
+            pastebinHomePage.SelectPasteExpiration("10 Minutes");
+            pastebinHomePage.EnterPasteName("helloweb");
+            pastebinHomePage.CreateNewPaste();
 
-            // Enter the code into the "New Paste" text area
-            IWebElement pasteCodeElement = driver.FindElement(By.Id("postform-text"));
-            pasteCodeElement.SendKeys(content);
+            // Assert
+            var createdPasteName = driver.FindElement(By.XPath("//div[@class='info-top']//h1")).Text;
+            Assert.Equal("helloweb", createdPasteName);
 
-            // Set the "Paste Expiration" dropdown to "10 Minutes"
-            driver.FindElement(By.Id("select2-postform-expiration-container")).Click();
-            driver.FindElement(By.XPath("//li[text()='10 Minutes']")).Click();
-
-            // Set the "Paste Name / Title" to "helloweb"
-            IWebElement pasteNameElement = driver.FindElement(By.Id("postform-name"));
-            pasteNameElement.SendKeys(title);
-
-            // Click the "Create New Paste" button
-            IWebElement createPasteButton = driver.FindElement(By.XPath("//button[contains(text(),'Create New Paste')]"));
-            createPasteButton.Click();
-
-            // Verify that the paste name is correct
-            IWebElement pasteTitle = driver.FindElement(By.XPath("//div[@class='info-top']//h1"));
-            Assert.Equal(title, pasteTitle.Text);
-
-            // Verify that expiration time is correct
-            IWebElement expirationTime = driver.FindElement(By.XPath("//div[@class='expire']"));
+            var expirationTime = driver.FindElement(By.XPath("//div[@class='expire']"));
             Assert.Equal("10 min", expirationTime.Text, StringComparer.OrdinalIgnoreCase);
 
-            // Verify that the content is correct
-            IWebElement pasteContent = driver.FindElement(By.XPath("//div[@class='de1']"));
-            Assert.Equal(content, pasteContent.Text);
+            var createdPasteCode = driver.FindElement(By.XPath("//div[@class='de1']")).Text;
+            Assert.Equal("Hello from WebDriver", createdPasteCode);
 
             driver.Close();
         }
